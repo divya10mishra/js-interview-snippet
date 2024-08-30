86eb450ff34aeccd407767680a9921e04759a5bf
Divya Mishra <43552180+divya10mishra@users.noreply.github.com>
2024-08-30 22:06:17 +0530

    Update eventloopSnippet.js

--- snippets/eventloopSnippet.js
+++ snippets/eventloopSnippet.js
@@ -33,3 +33,42 @@ new Promise((res, rej) => {
 });
 
 //1,12,15,8,7,6
+
+
+
+
+//====================snippet2=============================
+
+
+
+console.log(1);
+const pro = new Promise((resolve) => {
+  console.log(2);
+  resolve();
+  console.log(3);
+});
+(async function (x) {
+  console.log("IIFE", x);
+  x = await pro;
+  console.log("await", x);
+})("hello");
+console.log(4);
+process.nextTick(() => {
+  console.log("nextTick");
+});
+pro
+  .then((data) => {
+    console.log("data", data);
+    return data || "bad day";
+  })
+  .then((data) => {
+    console.log("then then", data);
+  });
+console.log(7);
+setTimeout(() => {
+  console.log(8);
+}, 10);
+setTimeout(() => {
+  console.log(9);
+}, 0);
+//output:  1,2,3,IIFE hello,4,7,nextTick,await undefined,data undefined,then then bad day,9,8
