7935eb7d6ce59b05d627bc735954742c8a18d687
Divya Mishra <43552180+divya10mishra@users.noreply.github.com>
2024-09-09 15:42:32 +0530

    Create ObjectTransform.js

---
+++ snippets/ObjectTransform.js
@@ -0,0 +1,71 @@
+const data = [{
+    name:'Zalice',
+    grades:{Maths:20,English:50,History:78}
+},
+{
+    name:'Bob',
+    grades:{Maths:20,English:50,History:78}
+},
+{
+    name:'Megha',
+    grades:{Maths:20,English:50,History:78}
+}]
+
+// output : {
+//     Maths:[{name:'Alice', grade: 20},{name:'Bob', grade: 20},{name:'Megha', grade: 20}],
+//     English:[{name:'Alice', grade: 20},{name:'Bob', grade: 20},{name:'Megha', grade: 20}],
+//     History:[{name:'Alice', grade: 20},{name:'Bob', grade: 20},{name:'Megha', grade: 20}]
+// }
+
+
+function transform(data){
+ let result = {}
+ 
+//  data.forEach(student=>Object.entries(student)
+//  .forEach(([sub,grade])=>{
+//      if(!result[sub]) result[sub] = []
+//      result[sub].push({name:student.name,grade:grade})
+//  }))
+
+data.reduce((acc,curr)=>{
+    Object.entries(curr.grades).forEach(([sub,grade])=>{
+        if(!acc[sub]){
+            acc[sub]=[]
+        }
+        acc[sub].push({name:curr.name, grade})
+    })
+    
+ //   console.log(acc,"acc")
+  
+    return acc
+},result)
+ 
+ console.log(result,"result")
+ 
+ let sorted = {}
+ 
+ Object.entries(result).map(([sub,obj])=> 
+ sorted[sub]=obj.sort((a,b)=>a.name.localeCompare(b.name))
+    )
+
+ 
+ console.log(sorted,"sorted")
+}
+
+transform(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
